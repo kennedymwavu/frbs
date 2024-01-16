3aa483617f16898c482bab03d5764ac436edfbb8
kennedymwavu <mwavukennedy@gmail.com>
2024-01-16 04:59:25 +0300

    ft: define error handler

---
+++ R/frbs_error_handler.R
@@ -0,0 +1,13 @@
+#' frbs error handler
+#'
+#' Used in the `tryCatch()` blocks of `frbs_*()` functions
+#' @param e The error.
+frbs_error_handler <- \(e) {
+  resp_err <- httr2::last_response()
+  if (!is.null(resp_err)) {
+    resp_err <- httr2::resp_body_json(resp_err)$error
+    err_list <- list(code = resp_err$code, message = resp_err$message)
+    return(err_list)
+  }
+  stop(conditionMessage(e), call. = FALSE)
+}
