fced6d481e58e9feb164e20fe02aea1f3b8ef7e2
kennedymwavu <mwavukennedy@gmail.com>
2024-01-18 04:13:03 +0300

    fix: return error as named list

--- R/frbs_error_handler.R
+++ R/frbs_error_handler.R
@@ -6,7 +6,12 @@ frbs_error_handler <- \(e) {
   resp_err <- httr2::last_response()
   if (!is.null(resp_err)) {
     resp_err <- httr2::resp_body_json(resp_err)$error
-    err_list <- list(code = resp_err$code, message = resp_err$message)
+    err_list <- list(
+      error = list(
+        code = resp_err$code,
+        message = resp_err$message
+      )
+    )
     return(err_list)
   }
   stop(conditionMessage(e), call. = FALSE)
